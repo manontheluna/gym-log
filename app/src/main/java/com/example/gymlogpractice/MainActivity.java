02684566df50259ca3fb43fa3829d92946749806
manontheluna <eluna-ortez@csumb.edu>
2025-11-30 17:26:47 -0800

    add necessary logic and components for login screen

--- app/src/main/java/com/example/gymlogpractice/MainActivity.java
+++ app/src/main/java/com/example/gymlogpractice/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.gymlogpractice;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
@@ -21,6 +23,7 @@ import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String MAIN_ACTIVITY_USER_ID = "com.example.gymlogpractice.MAIN_ACTIVITY_USER_ID";
     private ActivityMainBinding binding;
     private GymLogRepository repository;
 
@@ -38,6 +41,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        loginUser();
+        if (loggedInUserId == -1) {
+            Intent intent = LoginActivity.loginIntentFactory(getApplicationContext());
+            startActivity(intent);
+        }
 
         repository = GymLogRepository.getRepository(getApplication());
 
@@ -61,6 +70,16 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void loginUser() {
+        // TODO: create login method
+        loggedInUserId = getIntent().getIntExtra(MAIN_ACTIVITY_USER_ID, -1);
+    }
+
+    static Intent mainActivityIntentFactory(Context context, int userId) {
+        Intent intent = new Intent(context, MainActivity.class);
+        intent.putExtra(MAIN_ACTIVITY_USER_ID, userId);
+        return intent;
+    }
     private void insertGymLogRecord() {
         if (exercise.isEmpty()) {
             return;
